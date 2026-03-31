e704ee1ec78ec3ae234ce54825f8ddd7562bc296
Oykun Yanilmaz <yanilmaz.oykun@gmail.com>
2026-03-31 14:58:34 +0300

    Api: Add user avatar picture to the user edit in the admin panel

--- api/accounts/admin.py
+++ api/accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-
+from django.utils.safestring import mark_safe
 from .models import User
+
 
 
 @admin.register(User)
@@ -15,3 +16,23 @@ class UserAdmin(BaseUserAdmin):
             },
         ),
     )
+
+    fieldsets = BaseUserAdmin.fieldsets + (
+        (
+            "Profile Picture", 
+            {
+                "fields": ("avatar", "avatar_preview"),
+            }
+        ),
+    )
+
+    readonly_fields = ("avatar_preview",)
+
+    def avatar_preview(self, obj):
+        if obj.avatar:
+            return mark_safe(
+                f'<img src="{obj.avatar.url}" style="width: 80px; height: 80px; object-fit: cover; border-radius: 8px;" />'
+            )
+        
+        return "-"
+    
